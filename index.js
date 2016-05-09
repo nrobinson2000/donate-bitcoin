643ca1b39f7f3db0133a48f6f41fa2961966ae99
Nathan Robinson <nrobinson2000@me.com>
2016-05-09 11:10:22 +0100

    Keeping my streak up :) at 11:10 of 09-05-2016

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ var link = true; // Set to false to disable generating hyperlink
 
 function httpGet(theUrl){var xmlHttp = new XMLHttpRequest();xmlHttp.open( "GET", theUrl, false );xmlHttp.send( null );return xmlHttp.responseText;}
 
-function donate(){
+function donate()
+{
 var currency = parseInt(document.getElementById("donatebox").value);
-  var json = httpGet("https://api.bitcoinaverage.com/ticker/"+currency_code+"/");  // Get bitcoin price
-  var obj = JSON.parse(json);
-  var bitcoin_price = obj.ask;
-  var finalexchange = (currency / bitcoin_price).toFixed(8);
-  var url = "bitcoin:"+ address +"?amount=" + finalexchange;
+var json = httpGet("https://api.bitcoinaverage.com/ticker/"+currency_code+"/");  // Get bitcoin price
+var obj = JSON.parse(json);
+var bitcoin_price = obj.ask;
+var finalexchange = (currency / bitcoin_price).toFixed(8);
+var url = "bitcoin:"+ address +"?amount=" + finalexchange;
 if (link == true){document.getElementById("donatetext").innerHTML ="<br><a href='"+ url + "'> Please send " + finalexchange.toString() + " Bitcoin to " + address + "</a>";}
 if (popup == true){window.open(url);}
 if (qrcode == true){document.getElementById("qrcode").innerHTML = "";}
 if (qrcode == true){$('#qrcode').qrcode(url);}
-console.log(url);}
+console.log(url);
+}
 
 function setCurrency(){document.getElementById("donatebutton").value = "Donate "+currency_code;}
