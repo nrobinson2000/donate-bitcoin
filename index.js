06fa2e1baef31fe2c6e2b7623322aa3987b73e52
Nathan Robinson <nrobinson2000@me.com>
2016-06-10 11:48:09 +0100

    at 11:48 of 10-06-2016

--- index.js
+++ index.js
@@ -2,19 +2,36 @@
 
 var address = "PUT_ADDRESS_HERE"; // The bitcoin address to receive donations. Change to yours
 var popup = false; // Set to true if you want a popup to pay bitcoin
-var currency_code = "USD"; // Change to your preferred currency. Choose from https://api.bitcoinaverage.com/ticker/
-var qrcode = false; // Set to true for qr code
+var currency_code = "USD"; // Change to your default currency. Choose from https://api.bitcoinaverage.com/ticker/
+var qrcode = true; // Set to false to disable qrcode
 var link = true; // Set to false to disable generating hyperlink
+var organization = "Example"; //Change to your organization name
+
+var params = {};
+
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+
+if (params.currency){currency_code = params.currency;}
+console.log(params.amount);
+console.log(currency_code);
 
 function httpGet(theUrl){var xmlHttp = new XMLHttpRequest();xmlHttp.open( "GET", theUrl, false );xmlHttp.send( null );return xmlHttp.responseText;}
 
 function donate()
 {
-var currency = parseInt(document.getElementById("donatebox").value);
+if (params.amount) {var currency_value = params.amount;}
+else {var currency_value = parseInt(document.getElementById("donatebox").value);}
 var json = httpGet("https://api.bitcoinaverage.com/ticker/"+currency_code+"/");  // Get bitcoin price
 var obj = JSON.parse(json);
 var bitcoin_price = obj.ask;
-var finalexchange = (currency / bitcoin_price).toFixed(8);
+var finalexchange = (currency_value / bitcoin_price).toFixed(5);
 var url = "bitcoin:"+ address +"?amount=" + finalexchange;
 if (link == true){document.getElementById("donatetext").innerHTML ="<br><a href='"+ url + "'> Please send " + finalexchange.toString() + " Bitcoin to " + address + "</a>";}
 if (popup == true){window.open(url);}
