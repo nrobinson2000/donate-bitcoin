7839111501df86541f6919f15175f2b9b357778a
Nathan Robinson <nrobinson2000@me.com>
2017-11-02 19:14:42 -0400

    at 19:14 of 2017-02-11

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // donate-bitcoin Copyright (GPL) 2016  Nathan Robinson
 
-var address = "15EQPm5BFGbxkqjqAoZKhapwShTtzap9aj"; // The bitcoin address to receive donations. Change to yours
+var address = "PUT_ADDRESS_HERE"; // The bitcoin address to receive donations. Change to yours
 var popup = false; // Set to true if you want a popup to pay bitcoin
 var currencyCode = "USD"; // Change to your default currency. Choose from https://blockchain.info/ticker?cors=true
 var qrcode = true; // Set to false to disable qrcode
 var link = true; // Set to false to disable generating hyperlink
-var organization = "Nathan Robinson"; // Change to your organization name
+var organization = "Example"; // Change to your organization name
 var mbits = true; // Set to false to display bitcoin traditionally
 var defaultAmountToDonate = 5; // Default amount to donate
 var defaultCurrency = 'USD'; // Default currency to fallback
